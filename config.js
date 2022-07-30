ae351a842a7a9dc81e5dac4499e49977a3f9e3a9
Rongzhang <60769469+cecilia322@users.noreply.github.com>
2022-07-30 14:30:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.__PAW_CONFIG__ = {
-    googleApiKey: 'AIzaSyBoX_m8IQZO2Fq__4XgkIRADhVXTJwtsbs',
+    googleApiKey: 'AIzaSyDKUJ5ZKzbO8ydqLiE7tnMB7SDXn73YLbw',
     googleSheetDocId: '1iM-lrdwvYRUaz2AEASGzeMrqX-M8wmZR',
 }
